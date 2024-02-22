a4ea637aa5dfbadb5aed800712f1bd53322a4b49
codesomehax <markiewicz.darek.dm@gmail.com>
2024-02-22 15:10:01 +0100

    Added Guardian from Heaven

---
+++ Assets/Scripts/Units/BattleUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Units
+{
+    public partial class BattleUnit : MonoBehaviour
+    {
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            SetupAnimator();
+        }
+    }
+}
