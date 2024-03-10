561887170365aa6eb00fdb58dae448d8f7c39660
codesomehax <markiewicz.darek.dm@gmail.com>
2024-03-10 15:15:21 +0100

    Added unit count images under the unit

---
+++ Assets/Scripts/Units/BattleUnit.Canvas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Units
+{
+    public partial class BattleUnit
+    {
+        private static readonly Vector3 HostCanvasPosition = new(0.3f, 0.5f, -0.3f);
+        private static readonly Vector3 ClientCanvasPosition = new(-0.3f, 0.5f, -0.3f);
+        private static readonly Color HostImageColor = new(140f / 255f, 22f / 255f, 22f / 255f);
+        private static readonly Color ClientImageColor = new(22f / 255f, 22f / 255f, 140f / 255f);
+        
+        [Header("Canvas")]
+        [SerializeField] private RectTransform canvasTransform;
+        [SerializeField] private Image image;
+        [SerializeField] private TMP_Text unitCountText;
+        
+        private Transform _cameraTransform;
+
+        private void AwakeCanvas()
+        {
+            canvasTransform.localPosition = IsHost ? HostCanvasPosition : ClientCanvasPosition;
+            image.color = IsHost ? HostImageColor : ClientImageColor;
+        }
+
+        // TODO make the size of the image constant regardless of the distance to the camera
+        private void RotateCanvasToCamera()
+        {
+            Vector3 canvasPosition = canvasTransform.position;
+            Vector3 cameraPosition = _cameraTransform.position;
+            Vector3 lookAtDirection = new(cameraPosition.x, cameraPosition.y, canvasPosition.z);
+            canvasTransform.rotation = Quaternion.LookRotation(lookAtDirection - canvasPosition);
+        }
+
+        private void SyncUnitCount(int prev, int next, bool asServer)
+        {
+            if (asServer) return;
+            unitCountText.text = next.ToString();
+        }
+    }
+}
--- Assets/Scripts/Units/BattleUnit.cs
+++ Assets/Scripts/Units/BattleUnit.cs
@@ -6,23 +6,35 @@ using UnityEngine;
 
 namespace Units
 {
-    public class BattleUnit : NetworkBehaviour
+    public partial class BattleUnit : NetworkBehaviour
     {
         public static event Action<Vector3Int> OnUnitPlaced;
         public static event Action<Vector3Int> OnUnitRemoval;
 
-        [SyncVar] [NonSerialized] public int Count;
+        [SyncVar(OnChange = nameof(SyncUnitCount))] [NonSerialized] public int Count;
         [SyncVar] [NonSerialized] public Vector3Int CellPosition;
 
         public string Name => battleUnitInfo.Name;
         public Faction Faction => battleUnitInfo.Faction;
         public UnitType UnitType => battleUnitInfo.UnitType;
         
+        [Header("Unit")]
         [SerializeField] private BattleUnitInfo battleUnitInfo;
+
+        private void Awake()
+        {
+            _cameraTransform = FindObjectOfType<Camera>().transform;
+        }
 
         public override void OnStartClient()
         {
+            AwakeCanvas();
             OnUnitPlaced?.Invoke(CellPosition);
+        }
+
+        private void Update()
+        {
+            RotateCanvasToCamera();
         }
 
         public override void OnStopClient()
